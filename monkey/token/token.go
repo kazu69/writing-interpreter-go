53ef50ddf511e24a6e5d181dbc26a7ddf6919b80
kazu69 <kazu69web@gmail.com>
2019-02-07 09:08:44 +0900

    add token if-else boolean

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -8,8 +8,13 @@ type Token struct {
 }
 
 var keyword = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 const (
@@ -43,6 +48,11 @@ const (
 	// キーワード
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 func LookupIdent(ident string) TokenType {
