ac8239c7a0716f65f220b48b030270bacb2d99e6
kazu69 <kazu69web@gmail.com>
2019-02-14 08:42:44 +0900

    catch error for parser

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/kazu69/golang-interpreter/monkey/ast"
 	"github.com/kazu69/golang-interpreter/monkey/lexer"
 	"github.com/kazu69/golang-interpreter/monkey/token"
@@ -11,15 +13,28 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	// 2つのtokenを読み込む
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("Expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -76,6 +91,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
